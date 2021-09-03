0f20b10dcc5e1906ad21f5d91a1529074066802e
Leland Curtis <lelandcurtis88@gmail.com>
2021-09-03 13:43:38 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Great Escape"
+is_entertaining = true
+movie = "True Lies"
+num_4 = 13
+num_6 = 189
+product = num_4 * num_6
+favorite_foods = ["burritos", "ravioli", "dumplings"]
+favorite_foods.shift()
+puts(movie.length) # I assume you want me to count spaces as characters.
