a5a0953bf38bbfb35a2da64ed1f9dbf929b4c41e
Leland Curtis <lelandcurtis88@gmail.com>
2021-09-03 13:52:55 -0600

    Add dinosaur features as hash to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+# create a hash variable for a dinosaur
+
+dinosaur_features = {
+  period = "Triassic",
+  diet = "herboviour",
+  weight_tons = 120,
+  avg_lifespan_yrs = 23,
+  is_awesome = true,
+  is_alive_today = false,
+  fav_foods = ["leafy plants", "tree leaves", "moss"],
+  predators = ["Big Tooth", "T-Rex", "Meteors"]   
+}
