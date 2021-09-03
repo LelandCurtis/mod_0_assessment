4a5d2944e45ee7c3f286f81a027bba936bb1741b
Leland Curtis <lelandcurtis88@gmail.com>
2021-09-03 13:56:49 -0600

    Add developer features as hash to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer_features = {
+  first_name = "Bob",
+  last_name = "Builder",
+  age = 33,
+  height_in = 190,
+  is_cool = true,
+  is_smart = true,
+  favorite_colors = ["red", "blue", "yellow"],
+  best_friends = ["Patrick", "Reinhardt", "Phil"]
+}
